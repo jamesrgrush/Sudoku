4f3bbed164e552c2d695a4ae18ec5d0147c51d3d
jimmy <jimmy@DESKTOP-4MTD2T7>
2018-04-23 15:41:09 -0400

    more buttons

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -19,7 +22,11 @@ public class Main extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		// TODO Auto-generated method stub
+		VBox root = new VBox();
+		HBox boardAndControls = new HBox();
 		GridPane board = new GridPane();
+		
+		
 
 
 		for (int blockColumn = 0; blockColumn < 3 ; blockColumn++) {
@@ -42,7 +49,36 @@ public class Main extends Application{
 
 		    }
 		}
-		primaryStage.setScene(new Scene(board));
+		boardAndControls.getChildren().add(board);
+		boardAndControls.getChildren().add(new Label("Select Difficulty:"));
+		boardAndControls.getChildren().add(new Button("Easy"));
+		boardAndControls.getChildren().add(new Button("Medium"));
+		boardAndControls.getChildren().add(new Button("Hard"));
+		boardAndControls.getChildren().add(new Label("Game Controls:"));
+		boardAndControls.getChildren().add(new Button("Check Answers"));
+		boardAndControls.getChildren().add(new Button("Hint"));
+		boardAndControls.getChildren().add(new Label("Select Number:"));
+		boardAndControls.getChildren().add(new Button("1"));
+		boardAndControls.getChildren().add(new Button("2"));
+		boardAndControls.getChildren().add(new Button("3"));
+		boardAndControls.getChildren().add(new Button("4"));
+		boardAndControls.getChildren().add(new Button("5"));
+		boardAndControls.getChildren().add(new Button("6"));
+		boardAndControls.getChildren().add(new Button("7"));
+		boardAndControls.getChildren().add(new Button("8"));
+		boardAndControls.getChildren().add(new Button("9"));
+
+
+
+
+
+
+
+
+
+		
+		root.getChildren().add(boardAndControls);
+		primaryStage.setScene(new Scene(root));
 		primaryStage.show();
 	}
 
