def80a59cac973b1acc9c2307be344fb2327d24a
jamesrgrush <35544008+jamesrgrush@users.noreply.github.com>
2018-04-24 16:43:12 -0400

    Add files via upload
    maybe updated

---
+++ Cell.java
@@ -0,0 +1,110 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Cell {
+	Random ran = new Random();
+	
+    private int row, column, metaRow, metaColumn, number;
+    private ArrayList<Integer> candidates = new ArrayList<Integer>();
+    
+    Cell(int row, int column) {
+        this.row = row;
+        this.column = column;
+        findMetaPos();
+        for (int i = 1; i < 10; i++) {
+            candidates.add(i);
+        }
+    }
+    
+    public int getRow() {
+        return row;
+    }
+    
+    public int getColumn() {
+        return column;
+    }
+    
+    public int getMetaRow() {
+        return metaRow;
+    }
+    
+    public int getMetaColumn() {
+        return metaColumn;
+    }
+    
+    public int getNumber() {
+        return number;
+    }
+    
+    public int getNumOfCandidates() {
+    		return candidates.size();
+    	
+    }
+    
+    public int getRandomCandidate() {
+    	try {
+        return candidates.get(ran.nextInt(candidates.size()));
+    	}
+    	catch (IllegalArgumentException ex) {
+    		System.out.println(this.getNumOfCandidates());
+    		for (int i = 0; i <= candidates.size(); i++) {
+    			System.out.println(candidates.get(i));
+    		}
+    		return 0;
+    	}
+    }
+    
+    public void setNumber(int number) {
+        this.number = number;
+    }
+    
+    public void findMetaPos() {
+       if (row <= 2) {
+           metaRow = 0;
+        }
+       
+       else if (row <= 5) {
+           metaRow = 1;
+        }
+        
+        else {
+            metaRow = 2;
+        }
+        
+        if (column <= 2) {
+           metaColumn = 0;
+        }
+       
+       else if (column <= 5) {
+           metaColumn = 1;
+        }
+        
+        else {
+            metaColumn = 2;
+        }
+    }
+    
+    public boolean isEmpty() {
+        return number == 0;
+    }
+    
+    public void updateRemove(int usedCandidate) {
+    	if (candidates.contains(usedCandidate)) {
+    		candidates.remove(candidates.indexOf(usedCandidate));
+    	}
+    }
+    
+    public void updateAdd(int prevCandidate) {
+    	if (candidates.contains(prevCandidate) == false) {
+    		candidates.add(prevCandidate);
+    	}
+    }
+    
+    
+    public void updateRenew() {
+    	candidates.clear();
+    	 for (int i = 1; i < 10; i++) {
+             candidates.add(i);
+    	 }
+    }
+}
